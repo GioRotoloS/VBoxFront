4f97e803d5d9df70ebe827f06602da0348f99940
GRSilano <115807849+GRSilano@users.noreply.github.com>
2023-02-15 23:50:55 -0400

    commit front

---
+++ src/views/home/Config.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../../components/Navbar';
+
+const Config = () => {
+    return(
+        <div className="config">
+        <>
+            <Navbar/>
+            </>
+            <h3>Configuraciones</h3>
+    </div>
+    )
+}
+
+export default Config
